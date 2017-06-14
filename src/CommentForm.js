6d923bcdc5d758c766955ca2d2aaf9c6924afdf1
Kyle Brown <brow1144@purdue.edu>
2017-06-14 09:14:36 -0400

    Change some formatting and style of commentForm

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,11 +1,4 @@
-/*
-    TODO: Make Time Stamps Be the time and not switch all
-    TODO: Add some style to the list 
-    TODO: 
-*/
-
-
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './CommentForm.css'
 
 class CommentForm extends Component {
@@ -17,12 +10,11 @@ class CommentForm extends Component {
         comments: [],
       }
 
-      this.onSubmitFunction = this.onSubmitFunction.bind(this);
+      this.onSubmitFunction = this.onSubmitFunction.bind(this)
     }
 
     onSubmitFunction(ev) {
         ev.preventDefault()
-
         const comments = [...this.state.comments]
         comments.push(this.commentInput.value)
         this.setState({ comments })
@@ -30,12 +22,21 @@ class CommentForm extends Component {
     }
 
     renderComments(comment, i) {
-        return <li key={i}>{comment}<span>{new Date().toJSON().slice(0,10).replace(/-/g,'/')}</span></li>
+        return ( 
+            <li key={i}>
+                {comment}
+                <span>
+                    {new Date().toJSON().slice(0,10).replace(/-/g,'/')}
+                </span>
+            </li>
+
+
+        )
     }
     
     render() {
         return (
-            <form onSubmit={this.onSubmitFunction.bind(this)}>
+            <form className="commentForm" onSubmit={this.onSubmitFunction.bind(this)}>
                 <ul className="no-bullet">
                     {this.state.comments.map(this.renderComments)}
                 </ul>
